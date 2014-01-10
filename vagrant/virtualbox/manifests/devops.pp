d3daeb69acb9500e9fbb4087a311c7a444a9f5a4
srscruz <sergio.cruz@sages4it.com>
2014-01-09 22:07:44 -0200

    Update devops.pp

--- vagrant/virtualbox/manifests/devops.pp
+++ vagrant/virtualbox/manifests/devops.pp
@@ -1,4 +1,3 @@
-
   group { 'puppet':
     ensure => present
   }
@@ -7,46 +6,46 @@
 
   File { owner => 0, group => 0, mode => 0644 }
 
-  # Faz o Update do S.O
+  # Update O.S
   class {'apt':
     always_apt_update => true,
     require   =>  Exec['repo-juju']
   }
 
-  # Add Repositorio de Pacotes do JuJu
+  # Add Repository JuJu packages 
   exec { 'repo-juju':
     command   =>  'add-apt-repository ppa:juju/stable',
     path      => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/']
   }
 
-  # Copia Chaves do Usuario para o root
+  # copy of user key for root
   exec  { 'copy-keys':
     command   =>  'cp -pR /home/vagrant/.ssh /root/',
     require   =>  Package['build-essential', 'vim', 'curl', 'git-core', 'juju-local', 'linux-image-generic-lts-raring', 'linux-headers-generic-lts-raring']
   }
 
-  # Iniciar Juju
+  # Start Juju
   exec  { 'juju-init':
     command   =>  "sudo su -c \"juju init\"",
     path      => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
     require   =>  Class['apt']
   }
 
-  # Altera para Environment Local
+  # Change for environment Local
   exec  { 'juju-switch-local':
     command   => "sudo su -c \"juju switch local\"",
     path      => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
     require   => Exec['juju-init']
   }
 
-  # Faz o bootstrap do JujU
+  # Run bootstrap JujU
   exec  { 'juju-bootstrap':
     command   => "sudo su -c \"juju bootstrap\"",
     path      => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
     require   => Exec['juju-switch-local']
   }
 
-  # Carrega Modulo da App no Kernel
+  # Loads module App on Kernel
   exec  { 'apparmor_parser':
     command   =>  "sudo su -c \"apparmor_parser -R /etc/apparmor.d/usr.bin.lxc-start\" && sudo su -c \"ln -s /etc/apparmor.d/usr.bin.lxc-start /etc/apparmor.d/disable \"",
     path      =>  [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
